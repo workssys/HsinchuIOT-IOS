d36f56dfa1afbd138431646b982e3ba4db2ea52c
Chandler Li <chandlerli@Ivens-MacBook-Pro.local>
2015-12-08 02:28:40 +0800

    init commit

---
+++ HsinchuIOT/HsinchuIOT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HsinchuIOT
+//
+//  Created by Chandler Li on 12/8/15.
+//  Copyright © 2015 SL Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
